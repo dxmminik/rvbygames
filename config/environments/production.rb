459697091fa70238cc4ef508158115b7796fd44b
dxmminik <dominsliwinski@gmail.com>
2021-01-04 21:46:10 +0100

    dodanie endpointa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_region => ENV['AWS_REGION'],
+    :endpoint => ENV['AWS_HOST'],
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],

d198cba41f79d833b35bd0da62297e0dab43fff8
dxmminik <dominsliwinski@gmail.com>
2021-01-05 14:37:26 +0100

    wersja 1849334

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,13 +37,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'rvbygames.herokuapp.com' }
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_region => ENV['AWS_REGION'],
-    :endpoint => ENV['AWS_HOST'],
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename',
+    s3_host_name: 's3-us-east-2.amazonaws.com'
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION')
     }
   }
 end

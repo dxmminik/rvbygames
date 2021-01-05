12968359f4ec5821ab56b772ec9e53caf6ef8759
dxmminik <dominsliwinski@gmail.com>
2021-01-05 14:45:44 +0100

    DZIAŁA <3

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@
 
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+  validates_attachment_content_type :image, content_type: %r{\Aimage/.*\Z}
 end

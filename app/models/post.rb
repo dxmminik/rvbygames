0e1ba3b64938cc98e4407b98a096cc4e51e399fe
dxmminik <dominsliwinski@gmail.com>
2021-01-07 17:24:11 +0100

    99,5%

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@
 
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :image, styles: { medium: '600x300', thumb: '200x100' }
   validates_attachment_content_type :image, content_type: %r{\Aimage/.*\Z}
 end

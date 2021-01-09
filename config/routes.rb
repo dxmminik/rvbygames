884aaf5cdd7d61e892a3e8c7a960924cbb5e75eb
dxmminik <dominsliwinski@gmail.com>
2021-01-09 13:33:56 +0100

    1.0.3-admin_panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :posts
   devise_for :users
   root 'posts#index'
